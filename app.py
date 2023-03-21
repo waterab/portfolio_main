1aa48b026c7d1b92badef53adb14aec39cdf1b5c
waterab <49354198+waterab@users.noreply.github.com>
2023-03-21 09:36:54 -0500

    change db path for upload

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from sqlalchemy import create_engine
 import os
 
 #path = os.environ.get('DATABASE_URL_POSTGRES')
-engine = create_engine("sqlite:///C:\\Users\\WaterDev\\Documents\\GitHub\\portfolio_main\\hockeyviewer\\db_temp\\db2.db")
+# local engine
+# engine = create_engine("sqlite:///C:\\Users\\WaterDev\\Documents\\GitHub\\portfolio_main\\hockeyviewer\\db_temp\\db2.db")
+# package engine
+engine = create_engine('sqlite:///' + os.getcwd() + '/hockeyviewer/db_temp/db2.db')
+
 
 app = dash.Dash(
     __name__,
